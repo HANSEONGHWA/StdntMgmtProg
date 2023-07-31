735d13d3c0c17cde02abf30d018ce074dd151f03
HANSEONGHWA <ghk9206@hanmail.net>
2023-08-01 04:02:21 +0900

    11버전으로 변경

--- src/main/java/com/example/stdntmgmtprog/entity/Expense.java
+++ src/main/java/com/example/stdntmgmtprog/entity/Expense.java
@@ -1,6 +1,5 @@
 package com.example.stdntmgmtprog.entity;
 
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
--- src/main/java/com/example/stdntmgmtprog/entity/Teacher.java
+++ src/main/java/com/example/stdntmgmtprog/entity/Teacher.java
@@ -1,12 +1,10 @@
 package com.example.stdntmgmtprog.entity;
-
 import lombok.Getter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 
 @Entity
 @Getter
